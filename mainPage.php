e85a5c57e6b6778de29e9a3a2c7721c9de914d60
Voon <voonshunzhi@gmail.com>
2018-05-13 17:08:00 +0800

    Done

---
+++ mainPage.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>WebSong.co</title>
+    <link rel="stylesheet" href="../CSS_WEB_DESIGN/normalize.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/mainPage.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+    <div class="mainContent">
+        <div class="container content">
+           <div class="header">
+               <h1 class="headerTitle">Welcome to WebSong.co</h1>
+           </div>
+           <div class="albumSelectButton">
+               <div class="row">
+                    <!-- Load album available for selection from database  -->
+                    <?php
+                        
+                    ?>
+               </div>
+           </div>
+        </div>
+        <div class="playSongContainer">
+            <div class="row">
+                <div class="col-lg-4 songInfo">
+                    
+                </div>
+                <div class="col-lg-4 songControButtons">
+                    
+                </div>
+                <div class="col-lg-4 songVolumeControlButton">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+</body>
+</html>
